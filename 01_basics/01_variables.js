1baaa7d1cf40e75d6c916ac357959545fc66a954
sivammohapatra1310 <121796805+sivammohapatra1310@users.noreply.github.com>
2024-05-31 17:00:47 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const acctId=6969
+let acctEmail="sivam@gmail.com"
+var acctPass ="12345" //forget
+acctCity= "Bbsr"
+let acctState
+/*
+    dont use var cos of scope
+*/
+// acctId=2  //error
+
+console.log(acctId);
+console.table([acctId, acctPass, acctCity, acctState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// alert(3+3) // we are using nodejs, not browser
+// code readability should be high
+// number bigint string boolean null undefined symbol
+// object
+console.log(typeof("Sivam"))
+//  null = object
+//  undefined = undefined 
