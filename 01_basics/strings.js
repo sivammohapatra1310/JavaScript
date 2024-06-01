6fbe2f51a9ff70a2c7dd0d16a9f6701e9bc2de0f
sivammohapatra1310 <121796805+sivammohapatra1310@users.noreply.github.com>
2024-06-01 06:56:38 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,24 @@
+/* dont use name as a global variable*/
+const namee="Sivam"
+const rep=7
+console.log(namee);
+console.log(`My name is ${namee} ${rep}`);
+
+//creating a String object
+const game=new String('Sivam')
+console.log(game);
+/*
+game[0] S
+game.charAt(1) i
+game.toUpperCase()
+game.indexOf()
+game.__proto__
+game.length
+game.slice()
+game.substring(start, end)
+game.trim //removes whitespaces and \n
+game.replace('old', 'new') 
+game.includes('sivam')
+game.split('i')
+*/
+console.log(game.fontcolor);
