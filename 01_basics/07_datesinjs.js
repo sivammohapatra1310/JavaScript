136404bcdef32daf58d06df2c4284fed0b744aa9
sivammohapatra1310 <121796805+sivammohapatra1310@users.noreply.github.com>
2024-06-02 09:10:55 +0000

    basics done 2:40 pm 2 June

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -1,9 +1,9 @@
 //console.log(Temporal.Now.instant);
 let myDate= new Date()
 console.log(typeof myDate);
-console.log(myDate.toString());
+//console.log(myDate.toString());
 console.log( myDate.toJSON());
-console.log(myDate.getDate());
+//console.log(myDate.getDate());
 let myCreatedDate=new Date("2024-01-06")
 myCreatedDate=new Date("01-06-2024")
 console.log(myCreatedDate)
@@ -12,4 +12,16 @@ Date(year: number, monthIndex: number,
 date?: number | undefined, hours?: number | undefined,
 minutes?: number | undefined, seconds?: number | undefined, 
 ms?: number | undefined): Date
+
+month starts from 0, 1, ... , 11
 */
+let mytimestamp=Date.now() //gives time in miliseconds
+console.log(Math.floor(mytimestamp/1000)) //seconds
+console.log(mytimestamp)
+console.log(myCreatedDate.getTime())
+
+let newDate=new Date()
+console.log(newDate.getMonth() +1 );
+
+//ctrl + space
+console.log(newDate.toLocaleString("en-US", { hour12: false }));
