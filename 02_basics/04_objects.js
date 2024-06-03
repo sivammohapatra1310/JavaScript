5105510b60dbc01edd545fdbe7f5208deaebf040
sivammohapatra1310 <121796805+sivammohapatra1310@users.noreply.github.com>
2024-06-03 18:02:34 +0000

    ob

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,3 +5,27 @@ const user1=new Object();
 console.log(user1);
 const user2={s1:{s2:{s3:4}}}
 console.log(user2.s1.s2);
+
+//combining 2 objects
+//const obj3=Object.assign({}, obj1, obj2)
+//ibj3={...ibj1, ...ibj3} -->spread operator
+
+//array of objects arr[ind].attr
+
+const mySym= Symbol("k1")
+const User={
+    name: "Sivam",
+    age:20,
+    location:"Bbsr",
+    email:"si@fmail.com",
+    ids: false,
+    last: ["Mon", "Don"]
+}; // similar to map
+
+//similar to python
+
+console.log(Object.keys(User));
+//vvimp in database
+console.log(Object.values(User));
+console.log(Object.entries(User));
+
