32505641d14e3ea3d6c7258262a51a99821ce9ff
sivammohapatra1310 <121796805+sivammohapatra1310@users.noreply.github.com>
2024-06-11 09:17:29 +0000

    learn llm

---
+++ 05_iterations/01_loop.js
@@ -0,0 +1,13 @@
+const arr = [3, 5, 7];
+arr.foo = "hello";
+
+for (const i in arr) {
+  console.log(i);
+}
+// "0" "1" "2" "foo"
+
+for (const i of arr) {
+  console.log(i);
+}
+// Logs: 3 5 7
+// 5 7 9 remaining
